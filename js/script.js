c9a9c013691cafe6caa367001e20154af5415f1f
duttachandan <duttaankit917@gmail.com>
2026-02-02 17:45:05 +0530

    update coutner

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
-const circle = document.querySelector(".circle");
+// Luxy Initializing
+luxy.init({
+  wrapper: "#luxy",
+  targets: ".luxy-el",
+  wrapperSpeed: 0.08,
+});
+
+// Code of Subititle Section
 const subtitle = document.querySelector(".subtitle");
 const subtitleText = subtitle.textContent;
-
 subtitle.innerText = "";
-
 subtitleText.split("").map((e, index) => {
   const middleIndex = Math.ceil(subtitleText.split("").length / 2);
   if (middleIndex > index) {
@@ -25,23 +30,44 @@ subtitleText.split("").map((e, index) => {
   }
 });
 
-window.addEventListener("load", () => {
-  const text = document.querySelector(".text");
-  text.innerHTML = text.innerText
-    .split("")
-    .map(
-      (char, i) =>
-        `<span style="transform:rotate(${i * 10.3}deg)">${char}</span>`,
-    )
-    .join("");
-});
+// Coutner Code
+const counterWinerSec = document.querySelector(".counter-winer-sec");
 
-luxy.init({
-  wrapper: "#luxy",
-  targets: ".luxy-el",
-  wrapperSpeed: 0.08,
-});
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((elm) => {
+      // if its visible in the webpage only then pass through or else return
+      if (!elm.isIntersecting) return;
 
+      // Only run if user came from outside viewport
+      if (elm.boundingClientRect.top > window.innerHeight) return;
+
+      let dynamicCounter = elm.target.getElementsByClassName("dynamic-counter");
+      console.log(dynamicCounter);
+      for (let i = 0; i < dynamicCounter.length; i++) {
+        let elem = dynamicCounter[i];
+        let count = 0;
+        let Target = elem.innerText;
+        elem.innerText = count;
+        let interVal = setInterval(() => {
+          count++;
+          elem.innerText = count;
+          if (count >= Target) {
+            clearInterval(interVal);
+          }
+          observer.unobserve(elm.target);
+        }, 20);
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  },
+);
+
+observer.observe(counterWinerSec);
+
+// Partner Section Swiper
 const partner_swiper = new Swiper(".partner-swiper", {
   slidesPerView: "auto",
   spaceBetween: 40,
@@ -72,6 +98,7 @@ const partner_swiper = new Swiper(".partner-swiper", {
   // },
 });
 
+// Gallery Swiper Section
 const Gallery_Swiper = new Swiper(".our-gallery-swiper", {
   loop: true,
   centeredSlides: false,
